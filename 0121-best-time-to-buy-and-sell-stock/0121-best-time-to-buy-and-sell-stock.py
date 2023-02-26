552f75a02bedca425dbef39ce49a3c9c62a44f8e
Dushyant Mahajan <dummahajan@gmail.com>
2023-02-26 11:58:32 -0500

    Time: 1070 ms (67.12%), Space: 25.1 MB (5.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -13,16 +13,52 @@ class Solution:
         return maxp
 
 
-    def nsquare_time_constant_space(self, a: List[int]) -> "[sellday,buyday,work]":
-            n = len(a)
-            buyday = sellday = 0
-            work = profit = 0
-            for i in range(n):
-                for j in range(i, n):
-                    if a[j] - a[i] > profit:
-                        profit = a[j] - a[i]
-                        buyday, sellday = i, j
-                    work += 1
-            sol =[sellday, buyday, work]
-            return sol
+    def _nlogn_time_logn_space(self, a: List[int]) -> "[sellday,buyday,work]":
+       
+        a = self.divide_n_conquer(a, 0, len(a)-1)
         
+        sell_day = a[2]
+        buy_day = a[1]
+        work = a[0]
+        return [sell_day, buy_day, work]
+    
+    def divide_n_conquer(self, arr, start, stop):
+        n = stop - start
+
+        if n == 0:
+            return 0, start, start
+
+        if n == 1:
+            return arr[stop] - arr[start], start, stop
+
+        mid = int(start + n/2)
+
+        max_profit_left, buy_left, sell_left = self.divide_n_conquer(arr, start, mid-1)
+        max_profit_right, buy_right, sell_right = self.divide_n_conquer(arr, mid, stop)
+
+        buy_index = start
+        mp_buy_val = arr[start]
+        for k in range(start+1, mid):
+            if arr[k] < mp_buy_val:
+                mp_buy_val = arr[k]
+                buy_index = k
+
+        sell_index = mid
+        mp_sell_val = arr[mid]
+        for k in range(mid+1, stop+1):
+            if arr[k] > mp_sell_val:
+                mp_sell_val = arr[k]
+                sell_index = k
+
+        max_profit_ttl = mp_sell_val - mp_buy_val
+
+        if max_profit_right > max_profit_left:
+            if max_profit_ttl > max_profit_right:
+                return max_profit_ttl, buy_index, sell_index
+            else:
+                return max_profit_right, buy_right, sell_right
+        else:
+            if max_profit_ttl > max_profit_left:
+                return max_profit_ttl, buy_index, sell_index
+            else:
+                return max_profit_left, buy_left, sell_left
