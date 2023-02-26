ef845cd19d3d6135b37934b2a8492d82c0c4ce30
Dushyant Mahajan <dummahajan@gmail.com>
2023-02-26 11:57:36 -0500

    Time: 1115 ms (51.29%), Space: 25.1 MB (32.74%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,28 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        p1 = 0
-        p2 = 1
-        max_profit = 0
-        while p2 < len(prices):
-            if prices[p1]>prices[p2]:
-                p1 = p2
-            elif max_profit< prices[p2]-prices[p1]:
-                max_profit = prices[p2]-prices[p1]
-            p2 +=1
-        return max_profit
-                
-            
+        l, r = 0, 1
+        maxp = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxp = max(profit, maxp)
+            else:
+                l = r
+            r += 1
+        return maxp
+
+
+    def nsquare_time_constant_space(self, a: List[int]) -> "[sellday,buyday,work]":
+            n = len(a)
+            buyday = sellday = 0
+            work = profit = 0
+            for i in range(n):
+                for j in range(i, n):
+                    if a[j] - a[i] > profit:
+                        profit = a[j] - a[i]
+                        buyday, sellday = i, j
+                    work += 1
+            sol =[sellday, buyday, work]
+            return sol
+        
